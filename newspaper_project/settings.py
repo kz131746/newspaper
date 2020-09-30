2225a2e8344587c10a1cd6e231a8a029b7f1e6e7
kz131746 <kendra.zuleger@umconnect.umt.edu>
2020-09-30 10:18:51 -0600

    halfway through part 4 of newspaper app

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'articles.apps.ArticlesConfig',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
